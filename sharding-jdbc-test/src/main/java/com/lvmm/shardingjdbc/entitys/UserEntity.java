20644838c737cb9f9d3b48a1046f23c11649b985
zhouzhengzheng <zhouzhengzheng@lvmama.com>
2020-03-19 18:02:50 +0800

    用户查询

--- sharding-jdbc-test/src/main/java/com/lvmm/shardingjdbc/entitys/UserEntity.java
+++ sharding-jdbc-test/src/main/java/com/lvmm/shardingjdbc/entitys/UserEntity.java
@@ -2,10 +2,19 @@ package com.lvmm.shardingjdbc.entitys;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "user")
 public class UserEntity {
 
-    private  Long userId;
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    @Column(name = "user_name")
     private String userName;
+    @Column(name = "age")
     private int age;
 }
