7760db61ae35ab925be7969ef80b230b4a825271
zhouzhengzheng <zhouzhengzheng@lvmama.com>
2020-03-24 18:21:55 +0800

    redis

---
+++ sharding-jdbc-test/src/main/java/com/lvmm/shardingjdbc/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.lvmm.shardingjdbc.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@PropertySource(value = "redis.properties")
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(factory);
+
+        // 修改rdis默认序列化方式为StringRedisSerializer
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(stringRedisSerializer);
+
+        return redisTemplate;
+    }
+}
--- sharding-jdbc-test/src/main/java/com/lvmm/shardingjdbc/service/Imp/OrderServiceImp.java
+++ sharding-jdbc-test/src/main/java/com/lvmm/shardingjdbc/service/Imp/OrderServiceImp.java
@@ -4,6 +4,7 @@ import com.lvmm.shardingjdbc.entitys.OrderEntity;
 import com.lvmm.shardingjdbc.respository.OrderRespository;
 import com.lvmm.shardingjdbc.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,9 +13,12 @@ import java.util.Optional;
 public class OrderServiceImp implements OrderService {
     @Autowired
     private OrderRespository orderRespository;
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
     @Override
     public OrderEntity findByOrderId(Long orderId) {
         Optional<OrderEntity> order = orderRespository.findById(orderId);
+        redisTemplate.opsForValue().set("username","zhouzhengzheng");
         System.out.println("hello");
         System.out.println(order);
         return null;
