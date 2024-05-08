5986004c5d0d7f96e274db6c3105a273baa3092d
jakchou <zhouzhengzheng@able-elec.com>
2024-05-08 18:32:48 +0800

    Commit rm

--- src/main/resources/db/upgrade-defaultDS-mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `entity2` (
-`id` bigint NOT NULL COMMENT '主键',
-`created_time` datetime NULL COMMENT '创建时间',
-`updated_time` datetime NULL COMMENT '更新时间',
-`created_by` varchar(256) NULL COMMENT '创建者',
-`updated_by` varchar(256) NULL COMMENT '更新者',
-PRIMARY KEY (`id`)
-);
-
